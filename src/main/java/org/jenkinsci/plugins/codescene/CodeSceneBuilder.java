42a1f6a20834356d21a149d23030662d48a05d57
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-03-27 17:05:12 +0200

    Get a list of commits

--- src/main/java/org/jenkinsci/plugins/codescene/CodeSceneBuilder.java
+++ src/main/java/org/jenkinsci/plugins/codescene/CodeSceneBuilder.java
@@ -1,4 +1,5 @@
 package org.jenkinsci.plugins.codescene;
+import hudson.EnvVars;
 import hudson.Launcher;
 import hudson.Extension;
 import hudson.FilePath;
@@ -15,37 +16,24 @@ import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.QueryParameter;
 
 import javax.servlet.ServletException;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-/**
- * CodeSceneBuilder {@link Builder}.
- *
- * <p>
- * When the user configures the project and enables this builder,
- * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
- * and a new {@link CodeSceneBuilder} is created. The created
- * instance is persisted to the project configuration XML by using
- * XStream, so this allows you to use instance fields (like {@link #name})
- * to remember the configuration.
- *
- * <p>
- * When a build is performed, the {@link #perform} method will be invoked.
- */
 public class CodeSceneBuilder extends Builder implements SimpleBuildStep {
 
-    private final String name;
+    private final String baseRevision;
 
     // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
     @DataBoundConstructor
-    public CodeSceneBuilder(String name) {
-        this.name = name;
+    public CodeSceneBuilder(String baseRevision) {
+        this.baseRevision = baseRevision;
     }
 
     /**
      * We'll use this from the {@code config.jelly}.
      */
-    public String getName() {
-        return name;
+    public String getBaseRevision() {
+        return baseRevision;
     }
 
     @Override
@@ -53,100 +41,56 @@ public class CodeSceneBuilder extends Builder implements SimpleBuildStep {
         // This is where you 'build' the project.
         // Since this is a dummy, we just say 'hello world' and call that a build.
 
-        // This also shows how you can consult the global configuration of the builder
-        if (getDescriptor().getUseFrench())
-            listener.getLogger().println("Bonjour, "+name+"!");
-        else
-            listener.getLogger().println("Hello, "+name+"!");
+
+        try {
+            EnvVars env = build.getEnvironment(listener);
+            listener.getLogger().println(workspace);
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+            int code = launcher.launch()
+                    .cmdAsSingleString(String.format("git log --pretty='%%H' %s..%s", getBaseRevision(), env.get("GIT_COMMIT")))
+                    .pwd(workspace)
+                    .envs(build.getEnvironment(listener))
+                    .stdout(out)
+                    .join();
+
+            listener.getLogger().println("Exit: " + code);
+            listener.getLogger().println(out.toString());
+
+        } catch (InterruptedException e) {
+            listener.error("Failed to run git command", e);
+        } catch (IOException e) {
+            listener.error("Failed to run git command", e);
+        }
     }
 
-    // Overridden for better type safety.
-    // If your plugin doesn't really define any property on Descriptor,
-    // you don't have to do this.
-    @Override
-    public DescriptorImpl getDescriptor() {
-        return (DescriptorImpl)super.getDescriptor();
-    }
-
-    /**
-     * Descriptor for {@link CodeSceneBuilder}. Used as a singleton.
-     * The class is marked as public so that it can be accessed from views.
-     *
-     * <p>
-     * See {@code src/main/resources/org/jenkinsci/plugins/codescene/CodeSceneBuilder/*.jelly}
-     * for the actual HTML fragment for the configuration screen.
-     */
-    @Extension // This indicates to Jenkins that this is an implementation of an extension point.
+    @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
-        /**
-         * To persist global configuration information,
-         * simply store it in a field and call save().
-         *
-         * <p>
-         * If you don't want fields to be persisted, use {@code transient}.
-         */
-        private boolean useFrench;
-
-        /**
-         * In order to load the persisted global configuration, you have to 
-         * call load() in the constructor.
-         */
         public DescriptorImpl() {
             load();
         }
 
         /**
-         * Performs on-the-fly validation of the form field 'name'.
-         *
-         * @param value
-         *      This parameter receives the value that the user has typed.
-         * @return
-         *      Indicates the outcome of the validation. This is sent to the browser.
-         *      <p>
-         *      Note that returning {@link FormValidation#error(String)} does not
-         *      prevent the form from being saved. It just means that a message
-         *      will be displayed to the user. 
+         * This human readable name is used in the configuration screen.
          */
-        public FormValidation doCheckName(@QueryParameter String value)
-                throws IOException, ServletException {
-            if (value.length() == 0)
-                return FormValidation.error("Please set a name");
-            if (value.length() < 4)
-                return FormValidation.warning("Isn't the name too short?");
-            return FormValidation.ok();
+        @Override
+        public String getDisplayName() {
+            return "Run CodeScene Delta Analysis";
         }
 
-        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
-            // Indicates that this builder can be used with all kinds of project types 
+        /**
+         * Applicable to any kind of project.
+         */
+        @Override
+        public boolean isApplicable(Class type) {
             return true;
         }
 
-        /**
-         * This human readable name is used in the configuration screen.
-         */
-        public String getDisplayName() {
-            return "Say hello world";
-        }
-
         @Override
-        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
-            // To persist global configuration information,
-            // set that to properties and call save().
-            useFrench = formData.getBoolean("useFrench");
-            // ^Can also use req.bindJSON(this, formData);
-            //  (easier when there are many fields; need set* methods for this, like setUseFrench)
+        public boolean configure(StaplerRequest staplerRequest, JSONObject json) throws FormException {
             save();
-            return super.configure(req,formData);
-        }
-
-        /**
-         * This method returns true if the global configuration says we should speak French.
-         *
-         * The method name is bit awkward because global.jelly calls this method to determine
-         * the initial state of the checkbox by the naming convention.
-         */
-        public boolean getUseFrench() {
-            return useFrench;
+            return true; // indicate that everything is good so far
         }
     }
 }
